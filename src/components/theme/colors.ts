fb7cb8604fc33a9a50d6314c6e2812f329227f00
SimonePaglino <simpag99@gmail.com>
2023-06-18 02:20:13 +0200

    Update theme, sizes, colors

---
+++ src/components/theme/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  white: '#fff',
+  green: '#008000',
+  yellow: '#dcb000',
+  red: '#c9411f',
+  blue: 'rgb(2,62,231)',
+  'blue-disabled': '#023EE780',
+  'black-lighter': '#d5dadf',
+}
